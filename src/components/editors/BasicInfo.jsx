2d5c2ab54bcfad9c90e6f452f8ac12882b9b7f77
Renchester Ramos <renchesterjramos@gmail.com>
2023-03-04 06:52:15 +0800

    Add basic info input field

---
+++ src/components/editors/BasicInfo.jsx
@@ -0,0 +1,49 @@
+function BasicInfo() {
+  return (
+    <fieldset className="form-fieldset form__basic-info">
+      <legend className="form__legend">Basic Info</legend>
+      <label htmlFor="form-input__first-name" className="form-label">
+        First name:
+        <input
+          type="text"
+          name="firstName"
+          id="form-input__first-name"
+          className="form-input form-input__first-name"
+          placeholder="John"
+        />
+      </label>
+      <label htmlFor="form-last-name" className="form-label">
+        Last name:
+        <input
+          type="text"
+          name="lastName"
+          id="form-input__last-name"
+          className="form-input form-input__last-name"
+          placeholder="Appleseed"
+        />
+      </label>
+      <label htmlFor="form-input__occupation" className="form-label">
+        Professional title:
+        <input
+          type="text"
+          name="occupation"
+          id="form-input__occupation"
+          className="form-input form-input__occupation"
+          placeholder="Web developer"
+        />
+      </label>
+      <label htmlFor="form-input__self-description" className="form-label">
+        What do you do
+        <textarea
+          name="selfDescription"
+          id="form-input__self-description"
+          cols="30"
+          rows="10"
+          placeholder="Sample of what you do"
+        />
+      </label>
+    </fieldset>
+  );
+}
+
+export default BasicInfo;
