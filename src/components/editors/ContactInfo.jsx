e310c6ee9e54fba82ed675bff18557750e21e94b
Renchester Ramos <renchesterjramos@gmail.com>
2023-03-04 07:37:02 +0800

    Add contact info component

---
+++ src/components/editors/ContactInfo.jsx
@@ -0,0 +1,48 @@
+function ContactInfo() {
+  return (
+    <fieldset className="form-fieldset form__contact-info">
+      <legend className="form-legend">Contact Info</legend>
+      <label htmlFor="form-input__email" className="form-label">
+        Email:
+        <input
+          type="email"
+          name="email"
+          id="form-input__email"
+          className="form-input form-input__email"
+          placeholder="johnappleseed@site.com"
+        />
+      </label>
+      <label htmlFor="form-input__phone-number" className="form-label">
+        Phone Number:
+        <input
+          type="number"
+          name="phoneNumber"
+          id="form-input__phone-number"
+          className="form-input form-input__phone-number"
+        />
+      </label>
+      <label htmlFor="form-input__location" className="form-label">
+        Location:
+        <input
+          type="text"
+          name="location"
+          id="form-input__location"
+          className="form-input form-input__location"
+          placeholder="London, UK"
+        />
+      </label>
+      <label htmlFor="form-input__linkedin" className="form-label">
+        Linkedin:
+        <input
+          type="text"
+          name="linkedin"
+          id="form-input__linkedin"
+          className="form-input form-input__linkedin"
+          placeholder="linkedin.com/johnappleseed"
+        />
+      </label>
+    </fieldset>
+  );
+}
+
+export default ContactInfo;
