afb4259b1143a51725fe321b458f7d4715f7b4a5
Renchester Ramos <renchesterjramos@gmail.com>
2023-03-04 08:59:54 +0800

    Add education info component

---
+++ src/components/editors/EducationInfo.jsx
@@ -0,0 +1,52 @@
+function EducationInfo() {
+  return (
+    <fieldset className="form-fieldset form__education-info">
+      <legend className="form-legend">Education Background</legend>
+
+      <label htmlFor="form-input__education-institution" className="form-label">
+        University/Institution/Organization:
+        <input
+          type="text"
+          name="institution"
+          id="form-input__education-institution"
+          className="form-input form-input__education-institution"
+          placeholder="University of London"
+        />
+      </label>
+      <label htmlFor="form-input__starting-year" className="form-label">
+        Starting Year:
+        <input
+          type="month"
+          name="startingYear"
+          id="form-input__starting-year"
+          className="form-input form-input__starting-year"
+          min="1900-01"
+          placeholder="2010"
+        />
+      </label>
+      <label htmlFor="form-input__graduating-year" className="form-label">
+        Graduating Year:
+        <input
+          type="month"
+          name="graduatingYear"
+          id="form-input__graduating-year"
+          className="form-input form-input__graduating-year"
+          min="1900-01"
+          placeholder="2014"
+        />
+      </label>
+      <label htmlFor="form-input__gpa" className="form-label">
+        GPA:
+        <input
+          type="text"
+          name="gpa"
+          id="form-input__gpa"
+          className="form-input form-input__gpa"
+          placeholder="3.9/4.0"
+        />
+      </label>
+    </fieldset>
+  );
+}
+
+export default EducationInfo;
