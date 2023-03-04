6fe036c3bba7bf2df58f36f8601fa90c8a3938fb
Renchester Ramos <renchesterjramos@gmail.com>
2023-03-05 02:59:44 +0800

    Add component to display completed items for education and work experience

---
+++ src/components/editors/CompletedBackgroundForm.jsx
@@ -0,0 +1,30 @@
+function CompletedBackgroundForm(props) {
+  const { handleDelete, handleEdit, mainText, subText, id } = props;
+
+  return (
+    <div className="form__completed-bg form__completed-editable">
+      <p className="form__list-item-text">
+        <span className="list-item__main-text">{mainText} /</span>
+        <span className="list-item-sub-text">{subText}</span>
+      </p>
+
+      <button
+        type="button"
+        className="btn btn__edit-item"
+        onClick={() => handleEdit(id)}
+      >
+        Edit
+      </button>
+
+      <button
+        type="button"
+        className="btn btn__delete-item"
+        onClick={() => handleDelete(id)}
+      >
+        Delete
+      </button>
+    </div>
+  );
+}
+
+export default CompletedBackgroundForm;
